df068d0263a80de3a8fb9b4bb8ac1a4ff0d283ec
D-Alex-Aurelio <162036296+D-Alex-Aurelio@users.noreply.github.com>
2025-09-23 14:30:49 +0800

    Project Initialization

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df_accounts = pd.read_csv('./data/accounts.csv')
+df_products = pd.read_csv('./data/products.csv')
+df_sales_pipeline = pd.read_csv('./data/sales_pipeline.csv')
+df_sales_team = pd.read_csv('./data/sales_teams.csv')
+
+
+
+
+
