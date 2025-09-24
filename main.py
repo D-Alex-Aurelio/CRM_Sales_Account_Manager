935d599f9dcbd0f44406343e4c17d89b30b56c49
D-Alex-Aurelio <162036296+D-Alex-Aurelio@users.noreply.github.com>
2025-09-25 00:31:49 +0800

    Added Closed deals under opportunity details

--- main.py
+++ main.py
@@ -30,13 +30,53 @@ st.subheader(f'{selected_account_info.iloc[0,1]}')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.write(f'{selected_account_info.iloc[1, 0]}: {selected_account_info.iloc[1, 1]}')
-    st.write(f'{selected_account_info.iloc[2, 0]}: {selected_account_info.iloc[2, 1]}')
-    st.write(f'{selected_account_info.iloc[3, 0]}: {selected_account_info.iloc[3, 1]}')
+    st.write(f'**{selected_account_info.iloc[1, 0]}:** {selected_account_info.iloc[1, 1]}')
+    st.write(f'**{selected_account_info.iloc[2, 0]}:** {selected_account_info.iloc[2, 1]}')
+    st.write(f'**{selected_account_info.iloc[3, 0]}:** {selected_account_info.iloc[3, 1]}')
 
 with col2:
-    st.write(f'{selected_account_info.iloc[4, 0]}: {selected_account_info.iloc[4, 1]}')
-    st.write(f'{selected_account_info.iloc[5, 0]}: {selected_account_info.iloc[5, 1]}')
-    st.write(f'{selected_account_info.iloc[6, 0]}: {selected_account_info.iloc[6, 1]}')
+    st.write(f'**{selected_account_info.iloc[4, 0]}:** {selected_account_info.iloc[4, 1]}')
+    st.write(f'**{selected_account_info.iloc[5, 0]}:** {selected_account_info.iloc[5, 1]}')
+    st.write(f'**{selected_account_info.iloc[6, 0]}:** {selected_account_info.iloc[6, 1]}')
 
+st.header('Opportunity Details')
+selected_pipeline = df_sales_pipeline.loc[df_sales_pipeline['account']==selected_account,:]
+selected_pipeline = selected_pipeline.replace({'GTXPro':'GTX Pro'})
+selected_pipeline_closed_deals = selected_pipeline.loc[selected_pipeline['deal_stage'].isin(['Won','Lost']),:]
+selected_pipeline_closed_deals = selected_pipeline_closed_deals.reset_index(drop=True)
+selected_pipeline_closed_deals.index = selected_pipeline_closed_deals.index + 1
+closed_deals_counts = len(selected_pipeline_closed_deals)
+st.subheader(f'Closed Deals (*{closed_deals_counts}*)')
+closed_deals_prop = 100*selected_pipeline_closed_deals['deal_stage'].value_counts(normalize=True)
+closed_deals_prop = closed_deals_prop.round(2).reset_index()
+closed_deals_prop['var'] = 'Closed Deals'
+closed_deals_prop.columns = ['Deal Stage', 'Proportion', 'var']
+st.write(f'**Win Rate:** {closed_deals_prop.iloc[0,1]} %')
+st.bar_chart(closed_deals_prop, x='var', y='Proportion',
+             color='Deal Stage', stack=True,
+             y_label=' ', horizontal=True)
 
+closed_deals_columns = ['opportunity_id','sales_agent','product','deal_stage','close_value']
+
+with st.expander('Details'):
+    with st.container(height=250, border=False):
+        for i, row in selected_pipeline_closed_deals.loc[:,closed_deals_columns].iterrows():
+            with st.container(border=True):
+                col1, col2 = st.columns([0.5,4])
+                col1.subheader(f'{i}.)')
+                row = row.reset_index()
+                row.columns = ['var', 'value']
+                row['var'] = row['var'].str.capitalize()
+                row['var'] = row['var'].str.replace('_',' ')
+                agent = row.iloc[1,1]
+                sales_filter = df_sales_team['sales_agent']==agent
+                manager = df_sales_team.loc[sales_filter, 'manager']
+                office = df_sales_team.loc[sales_filter, 'regional_office']
+                row.iloc[1,1] = f'{row.iloc[1,1]} (*{manager.iloc[0]}, {office.iloc[0]} Office*)'
+                product = row.iloc[2,1]
+                product_filter = df_products['product']==product
+                series = df_products.loc[product_filter, 'series']
+                row.iloc[2,1] = f'{row.iloc[2,1]} *({series.iloc[0]} Series)*'
+                row.iloc[4,0] = f'{row.iloc[4,0]} (USD)'
+                for j in range(len(row)):
+                    col2.write(f'**{row.iloc[j,0]}**: {row.iloc[j,1]}')
